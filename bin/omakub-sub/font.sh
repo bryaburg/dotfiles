3d23071759f3e96ad239a61b60c509c02c878f64
alloutnoob <bryanburgess95@hotmail.com>
2024-07-26 10:41:29 -0500

    update bin folder

--- bin/omakub-sub/font.sh
+++ bin/omakub-sub/font.sh
@@ -24,25 +24,19 @@ set_font() {
 if [ "$#" -gt 1 ]; then
 	choice=${!#}
 else
-	choice=$(gum choose "Cascadia Mono" "Fira Mono" "JetBrains Mono" "Meslo" "> Change size" "<< Back" --height 8 --header "Choose your programming font")
+	choice=$(gum choose "Hacker" "> Change size" "<< Back" --height 8 --header "Choose your programming font")
 fi
 
 case $choice in
-"Cascadia Mono")
-	set_font "CaskaydiaMono Nerd Font" "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip" "ttf"
-	;;
-"Fira Mono")
-	set_font "FiraMono Nerd Font" "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraMono.zip" "otf"
-	;;
-"JetBrains Mono")
-	set_font "JetBrainsMono Nerd Font" "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip" "ttf"
-	;;
-"Meslo")
-	set_font "MesloLGS Nerd Font" "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip" "ttf"
+"Hacker")
+	set_font "Hasklug Nerd Font" "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Hasklug.zip" "ttf"
 	;;
 "> Change size")
 	source $OMAKUB_PATH/bin/omakub-sub/font-size.sh
 	exit
+	;;
+"<< Back")
+	# Add any necessary commands or scripts to run when "<< Back" is chosen
 	;;
 esac
 
--- bin/omakub-sub/theme.sh
+++ bin/omakub-sub/theme.sh
@@ -1,5 +1,5 @@
-THEME_NAMES=("Tokyo Night" "Catppuccin" "Nord" "Everforest" "Gruvbox" "Kanagawa" "Rose Pine")
-THEME=$(gum choose "${THEME_NAMES[@]}" "<< Back" --header "Choose your theme" --height 10 | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')
+THEME_NAMES=("Nord")
+THEME=$(gum choose "${THEME_NAMES[@]}" "<< Back" --header "Choose your theme" --height 2 | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')
 
 if [ -n "$THEME" ] && [ "$THEME" != "<<-back" ]; then
 	cp $OMAKUB_PATH/themes/$THEME/alacritty.toml ~/.config/alacritty/theme.toml
@@ -9,10 +9,6 @@ if [ -n "$THEME" ] && [ "$THEME" != "<<-back" ]; then
 
 	source $OMAKUB_PATH/themes/$THEME/gnome.sh
 	source $OMAKUB_PATH/themes/$THEME/vscode.sh
-
-	# Forgo setting the Chrome theme until we might find a less disruptive way of doing it.
-	# Having to quit Chrome, and all Chrome-based apps, is too much of an inposition.
-	# source $OMAKUB_PATH/themes/$THEME/chrome.sh
 fi
 
 source $OMAKUB_PATH/bin/omakub-sub/menu.sh
