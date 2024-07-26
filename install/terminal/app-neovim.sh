d41da9400ce9f9b729828e06760e6211cd0d1aeb
alloutnoob <bryanburgess95@hotmail.com>
2024-07-26 10:00:46 -0500

    neovim update

--- install/terminal/app-neovim.sh
+++ install/terminal/app-neovim.sh
@@ -10,7 +10,7 @@ cd -
 # Only attempt to set configuration if Neovim has never been run
 if [ ! -d "$HOME/.config/nvim" ]; then
 	# Use LazyVim
-	git clone https://github.com/LazyVim/starter ~/.config/nvim
+	git clone https://github.com/bryaburg/nvim ~/.config/nvim
 
 	# Disable update notification popup in starter config
 	sed -i 's/checker = { enabled = true }/checker = { enabled = true, notify = false }/g' ~/.config/nvim/lua/config/lazy.lua
@@ -19,8 +19,8 @@ if [ ! -d "$HOME/.config/nvim" ]; then
 	mkdir -p ~/.config/nvim/plugin/after
 	cp ~/.local/share/omakub/configs/neovim/transparency.lua ~/.config/nvim/plugin/after/
 
-	# Default to Tokyo Night theme
-	cp ~/.local/share/omakub/themes/tokyo-night/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
+	# Default to Nord theme
+	cp ~/.local/share/omakub/themes/nord/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
 
 	# Enable default extras
 	cp ~/.local/share/omakub/configs/neovim/lazyvim.json ~/.config/nvim/lazyvim.json
